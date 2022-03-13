3f37f9aea1115e8b85734db6eb8c646b2f443532
Buğrahan Çamlıbel <camlibelbugrahan@gmail.com>
2022-03-13 22:59:53 +0300

    20. videonun sonu

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Blog
+
 
 data = {
     "blogs": [
@@ -36,26 +38,20 @@ data = {
 # BURDA: urls.py daki url lere karşılık gelecek fonksiyonları yazıyoruz.
 def index(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_home=True, is_active=True)
     }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_active=True)
     }
     return render(request, "blog/blogs.html", context)
 
 
 def blog_details(request, id):
-    # blogs = data["blogs"]
-    # selectedBlog = None
-    # for blog in blogs:
-    #     if blog["id"] == id:
-    #         selectedBlog = blog
-    blogs = data["blogs"]
-    selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
+    blog = Blog.objects.get(id=id)
     return render(request, "blog/blog-details.html", {
-        "blog": selectedBlog
+        "blog": blog
     })

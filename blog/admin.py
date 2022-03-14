2a059740e974133643fc4cfc6291a6fde4eb659a
Buğrahan Çamlıbel <camlibelbugrahan@gmail.com>
2022-03-14 17:40:58 +0300

    21. videonun sonu

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Blog, Category
+
 
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_active", "is_home")
+    list_editable = ("is_active", "is_home")
+    search_fields = "title"
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,9 @@ class Blog(models.Model):
     is_active = models.BooleanField(default=False)
     is_home = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.title}"
+
 
 class Category(models.Model):
     name = models.CharField(max_length=150)

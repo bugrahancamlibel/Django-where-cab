9729e8c4fb567f7036262e9c9bb96d58902da80b
Buğrahan Çamlıbel <camlibelbugrahan@gmail.com>
2022-03-15 15:17:30 +0300

    28. videonun sonu

--- blog/models.py
+++ blog/models.py
@@ -2,23 +2,8 @@ from django.db import models
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 
+
 # Create your models here.
-
-
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="blogs")
-    description = RichTextField()
-    is_active = models.BooleanField(default=False)
-    is_home = models.BooleanField(default=False)
-    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
-
-    def __str__(self):
-        return f"{self.title}"
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
 
 class Category(models.Model):
@@ -31,3 +16,21 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="blogs")
+    description = RichTextField()
+    is_active = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL) # kategori silindiğinde bu blog
+                                                                                # silinmez kategorisi null olur
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from blog.models import Blog
+from blog.models import Blog, Category
 
-
-data = {
-    "blogs": [
-        {
-            "id": 1,
-            "title": "komple web dev",
-            "image": "1.jpg",
-            "is_active": True,
-            "is_home": False,
-            "description": "perfect course"
-        },
-        {
-            "id": 2,
-            "title": "python",
-            "image": "1.jpg",
-            "is_active": True,
-            "is_home": True,
-            "description": "perfect course"
-        },
-        {
-            "id": 3,
-            "title": "django kursu",
-            "image": "1.jpg",
-            "is_active": False,
-            "is_home": True,
-            "description": "perfect course"
-        },
-    ]
-}
 
 
 # Create your views here.
@@ -38,14 +9,16 @@ data = {
 # BURDA: urls.py daki url lere karşılık gelecek fonksiyonları yazıyoruz.
 def index(request):
     context = {
-        "blogs": Blog.objects.filter(is_home=True, is_active=True)
+        "blogs": Blog.objects.filter(is_home=True, is_active=True),
+        "categories": Category.objects.all()
     }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True)
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories": Category.objects.all()
     }
     return render(request, "blog/blogs.html", context)
 
@@ -55,3 +28,6 @@ def blog_details(request, slug):
     return render(request, "blog/blog-details.html", {
         "blog": blog
     })
+
+def blogs_by_category(request, slug):
+    pass
